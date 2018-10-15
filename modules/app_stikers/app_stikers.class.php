f400cce54ce2d3be3c6c8e2002704d341023ef17
Александр <39259970+ZokkiMir@users.noreply.github.com>
2018-10-15 23:28:16 +0300

    Update app_stikers.class.php

--- modules/app_stikers/app_stikers.class.php
+++ modules/app_stikers/app_stikers.class.php
@@ -160,9 +160,9 @@ function usual(&$out) {
 * @access public
 */
  function delete_app_stikers_data($id) {
-  $rec=SQLSelectOne("SELECT * FROM app_stikers_data WHERE id='$id'");
+  $rec=SQLSelectOne("SELECT * FROM app_stikers_data WHERE ID='$id'");
   // some action for related tables
-  SQLExec("DELETE FROM app_stikers_data WHERE id='".$rec['id']."'");
+  SQLExec("DELETE FROM app_stikers_data WHERE ID='".$rec['ID']."'");
  }
 /**
 * Install
@@ -197,23 +197,13 @@ function usual(&$out) {
 app_stikers_data - 
 */
   $data = <<<EOD
-CREATE TABLE `app_stikers_data` (
-  `id` int(10) unsigned NOT NULL auto_increment,
-  `text` varchar(128) collate utf8_unicode_ci NOT NULL default '',
-  `name` varchar(60) collate utf8_unicode_ci NOT NULL default '',
-  `color` enum('yellow','blue','green') collate utf8_unicode_ci NOT NULL default 'yellow',
-  `xyz` varchar(20) collate utf8_unicode_ci NOT NULL default '',
-  `dt` timestamp NOT NULL default CURRENT_TIMESTAMP,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=27 ;
-
---
--- Dumping data for table `app_stikers_data`
---
-
-INSERT INTO `app_stikers_data` VALUES(1, 'This is the first note! Add yours from the button above..', 'Martin', 'yellow', '478x0x1', '2010-01-17 06:30:14');
-INSERT INTO `app_stikers_data` VALUES(2, 'The position of the app_stikers_data is saved with AJAX.', 'Martin', 'blue', '0x321x2', '2010-01-17 06:57:39');
-INSERT INTO `app_stikers_data` VALUES(3, 'The app_stikers_data are automatically deleted after an hour.', 'Martin', 'green', '311x41x3', '2010-01-17 06:57:39');
+ app_stikers_data: id int(10) unsigned NOT NULL auto_increment
+ app_stikers_data: text varchar(128) collate utf8_unicode_ci NOT NULL default ''
+ app_stikers_data: text varchar(128) collate utf8_unicode_ci NOT NULL default ''
+ app_stikers_data: color enum('yellow','blue','green') collate utf8_unicode_ci NOT NULL default 'yellow'
+ app_stikers_data: xyz varchar(20) collate utf8_unicode_ci NOT NULL default ''
+ app_stikers_data: dt timestamp NOT NULL default CURRENT_TIMESTAMP
+ app_stikers_data: UPDATED datetime
 EOD;
   parent::dbInstall($data);
  }
